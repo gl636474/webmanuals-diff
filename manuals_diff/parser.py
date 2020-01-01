ab9f8032470afc18f769f66e81c5b4cf15ffc318
gareth <gareth@gareths-mac-mini.local>
2020-01-01 18:32:19 +0000

    Version 0.2.1
    Added methods to parser to extract:
     * revision
     * date (if exists)
     * title
     * page number (if exists)

--- manuals_diff/parser.py
+++ manuals_diff/parser.py
@@ -18,17 +18,54 @@ class WebManualsPageParser:
         the other methods of this class."""
         self._d = pyquery.PyQuery(filename=filename)
     
-    def revision(self):
-        """Return the revision informaion from the page header."""
-        
-        # The last cell of the last row in the first table in the first DIV
-        # contains a string of the form "Issue 2 / Revision 10"
-        text = self._d("div:first tbody:first tr:last td:last").text()
-
-        # text() will preserve whitespace - of which there can be loads in HTML 
+    def _strip_whitespace(self, text: str):
+        """Removes all whitespace from start and end of text and replaces all
+        sequences of whitespace in the middle of the text with a single 'normal'
+        space."""
         text = text.strip()
-        
-        # Replace any sequences of whitespace in the middle of the string with
-        # just a single 'normal' space
         text = re.sub('[\s+]', ' ', text)
         return text
+
+    def revision(self):
+        """Return the revision informaion from the page header (bottom row,
+        right most cell)."""
+        
+        # The last cell of the last row in the first table
+        # contains a string of the form "Issue 2 / Revision 10"
+        text = self._d("table:first tbody tr:last td:last").text()
+
+        return self._strip_whitespace(text)
+
+    def title(self):
+        """Return the title from the page header (top row, middle/second cell - 
+        which has row span of 3)."""
+        
+        text = self._d("table:first tbody tr:first td:nth-child(2)").text()
+        
+        return self._strip_whitespace(text)
+    
+    def date(self):
+        """Return the date from the page header (top or second row, last cell).
+        May or may not be present. Searches for the cell containing 'Date' and
+        returns the content of the next cell. Returns None if no such cell."""
+        
+        date_header_cell = self._d("table:first tbody tr td:contains('Date')")
+        if date_header_cell:
+            text = date_header_cell.next('td').text()
+            return self._strip_whitespace(text)
+        else:
+            return None
+
+    
+    def page_number(self):
+        """Return the page number from the page header (top or second row, last
+        cell). May or may not be present. Searches for the cell containing
+        'Page' and returns the content of the next cell. Returns None if no such
+        cell exists."""
+        
+        date_header_cell = self._d("table:first tbody tr td:contains('Page')")
+        if date_header_cell:
+            text = date_header_cell.next('td').text()
+            return self._strip_whitespace(text)
+        else:
+            return None
--- webmanuals_download.py
+++ webmanuals_download.py
@@ -4,13 +4,21 @@ from pathlib import Path
 from manuals_diff import WebManualsServer
 from manuals_diff import WebManualsPageParser
 
+OMA_MANUAL_ID = 5563
+FSI_MANUAL_ID = 12657
+
+manual_id = FSI_MANUAL_ID
+
 dest_dir = Path("/Users/gareth/Documents/Programming/eclipse-workspace-python/Webmanuals Diff")
 
-server = WebManualsServer('gladd', 'gladd')
-downloader = server.get_manual(5563)
-downloader.download(dest_dir)
+#server = WebManualsServer('gladd', 'gladd')
+#downloader = server.get_manual(manual_id)
+#downloader.download(dest_dir)
 
 file = dest_dir / "pages" / "page00000006"
 
 p = WebManualsPageParser(file)
 print(p.revision())
+print(p.title())
+print(p.date())
+print(p.page_number())
