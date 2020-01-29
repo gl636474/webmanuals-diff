f79c0966ae851eea8ef0d4ec8e53147b3fc1c1f2
gareth <gareth@gareths-mac-mini.local>
2020-01-29 22:12:58 +0000

    Test script updated to be totally offline and use new helper methods.

--- webmanuals_download.py
+++ webmanuals_download.py
@@ -10,9 +10,9 @@ FSI_MANUAL_ID = 12657
 
 dest_dir = Path("/Users/gareth/Documents/Programming/eclipse-workspace-python/Webmanuals Diff")
 
-username = input("Username: ")
-password = input("Password: ")
-server = WebManualsServer(username, password, cache_dir=dest_dir)
+#username = input("Username: ")
+#password = input("Password: ")
+server = WebManualsServer(cache_dir=dest_dir, offline=True)
 
 start_time = time()
 
@@ -26,12 +26,22 @@ end_time = time()
 total_time = end_time - start_time
 print("Took {} seconds to download/check docs".format(total_time))
 
-file = oma_dir / "page00000015"
 
-p = WebManualsPageParser(file)
+# Now parse a page of a manual
+
+manual_id = OMA_MANUAL_ID
+downloader = oma_downloader
+page_number = 9
+
+
+
+file = downloader.get_page_file(page_number)
+page_id = downloader.manual_metadata.get_page_id(page_number)
+
+p = WebManualsPageParser(file, page_id, page_number, manual_id)
 print(p.revision())
 print(p.title())
 print(p.date())
 print(p.page_number())
 #print(p.sanitised_content())
-print(p.wiki_markup())
+print(p.sanitised_wiki_markup())
